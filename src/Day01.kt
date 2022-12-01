4e95d0013bf6c62ddd49cf1a7fc87b7121261e3c
Nuno Pontes <NunoPontes@users.noreply.github.com>
2022-12-01 13:28:23 +0000

    Day 1 Advent

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
 fun main() {
+    fun calculateList(input: List<String>): MutableList<Int> {
+        val mutableListCalories = mutableListOf<Int>()
+        mutableListCalories.add(0)
+        input.forEach { calorie ->
+            if (calorie.isNotBlank()) {
+                mutableListCalories[mutableListCalories.lastIndex] =
+                    mutableListCalories[mutableListCalories.lastIndex] + calorie.toInt()
+            } else {
+                mutableListCalories.add(0)
+            }
+        }
+        return mutableListCalories
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val mutableListCalories = calculateList(input)
+        return mutableListCalories.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val mutableListCalories = calculateList(input)
+        return mutableListCalories.sorted().takeLast(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val inputPart1 = readInput("Day01_part1")
+    val inputPart2 = readInput("Day01_part2")
+    println(part1(inputPart1))
+    println(part2(inputPart2))
 }
